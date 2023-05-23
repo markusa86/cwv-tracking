407870225042386245f6e4b9b4dcbfc88f8b7102
Markus A <github_m@t6e.de>
2023-05-23 15:39:42 +0200

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,33 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-XX12345X1X');
+
+function sendToGoogleAnalytics({name, delta, value, id}) {
+  if (delta > 0) {
+    delta = Math.round((delta + Number.EPSILON) * 1000) / 1000
+  }
+  console.log(name,delta);
+  gtag('event', name, {
+    value: delta, // Use `delta` so the value can be summed.
+    metric_id: id, // Needed to aggregate events.
+    metric_value: value, // Optional.
+    metric_delta: delta, // Optional.
+  });
+}
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://unpkg.com/web-vitals@3/dist/web-vitals.iife.js';
+  script.onload = function () {
+    webVitals.getCLS(sendToGoogleAnalytics);
+    webVitals.getFID(sendToGoogleAnalytics);
+    webVitals.getLCP(sendToGoogleAnalytics);
+    webVitals.getFCP(sendToGoogleAnalytics);
+    webVitals.getINP(sendToGoogleAnalytics);
+    webVitals.getTTFB(sendToGoogleAnalytics);
+  };
+  document.head.appendChild(script);
+})();
